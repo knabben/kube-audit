d0efcc6b710458332d754850cf69dd36bb2e9d32
Amim Knabben <amim.knabben@gmail.com>
2020-01-03 22:17:46 -0500

    Import structure

--- apiserver/main.go
+++ apiserver/main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"log"
 	"encoding/json"
-	"io/ioutil"
 	"net/http"
 
 	"context"
 	"github.com/go-kit/kit/endpoint"
 	httptransport "github.com/go-kit/kit/transport/http"
+	"k8s.io/api/auditregistration/v1alpha1"
 
 )
 
@@ -19,20 +19,39 @@ type AuditService interface {
 
 type auditService struct{}
 
-func (auditService) SaveAudit(s saveRequest) (string, error) {
+func (auditService) SaveAudit(s v1alpha1.AuditSinkList) (string, error) {
 	return "", nil
 }
 
-type saveRequest struct {
 
-}
 type saveResponse struct {
 
 }
 
+type AuditEvent struct {
+	RequestURI         string
+	Verb               string
+	Code               int32
+	User               string
+	ImpersonatedUser   string
+	ImpersonatedGroups string
+	Resource           string
+	Namespace          string
+	RequestObject      bool
+	ResponseObject     bool
+	AuthorizeDecision  string
+
+	AdmissionWebhookMutationAnnotations map[string]string
+	AdmissionWebhookPatchAnnotations    map[string]string
+}
+
+type saveRequest struct {
+	Items []AuditEvent
+}
+
 func makeSaveEndpoint(svc auditService) endpoint.Endpoint {
 	return func(_ context.Context, request interface{}) (interface{}, error) {
-		req := request.(saveRequest)
+		req := request.(v1alpha1.AuditSinkList)
 		svc.SaveAudit(req)
 		return saveResponse{}, nil
 	}
@@ -53,10 +72,16 @@ func main() {
 }
 
 func decodeSaveRequest(_ context.Context, r *http.Request) (interface{}, error) {
-	req := saveRequest{}
-	b, _ := ioutil.ReadAll(r.Body)
-	fmt.Println(string(b))
-	return req, nil
+	var request = saveRequest{}
+	//
+	//b, _:= ioutil.ReadAll(r.Body)
+	//fmt.Println(string(b))
+
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	fmt.Println(request.Items)
+	return request, nil
 }
 
 func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
